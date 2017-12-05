14f2d7b6ace23bc8dea32af09159ea4ed3f138c5
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-12-05 19:45:33 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,7 @@
 ls -la
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > $APPCENTER_SOURCE_DIRECTORY/.npmrc
+touch $APPCENTER_SOURCE_DIRECTORY/.npmrc
+echo "@ewgenius:registry=https://evkhra.pkgs.visualstudio.com/_packaging/ewgenius/npm/registry/" >> $APPCENTER_SOURCE_DIRECTORY/.npmrc
+echo "//evkhra.pkgs.visualstudio.com/_packaging/ewgenius/npm/registry/:_authToken=${NPM_TOKEN}" >> $APPCENTER_SOURCE_DIRECTORY/.npmrc
+echo "//evkhra.pkgs.visualstudio.com/_packaging/ewgenius/npm/:_authToken=${NPM_TOKEN}" >> $APPCENTER_SOURCE_DIRECTORY/.npmrc
 ls -la
 cat $APPCENTER_SOURCE_DIRECTORY/.npmrc
