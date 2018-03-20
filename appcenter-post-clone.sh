a064acc2289c886928c3e403f272e2fa1f6c5b36
Evgeniy Khramkov <evkhramk@microsoft.com>
2018-03-20 17:41:59 +0300

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,2 +1,11 @@
-echo "commit $BUILD_SOURCEVERSION"
-echo "token $GITHUB_TOKEN"
+curl -X POST \
+  https://api.github.com/repos/evkhramkov/reactnative-application/statuses/$BUILD_SOURCEVERSION \
+  -H 'Authorization: token $GITHUB_TOKEN' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -d '{
+	"state": "pending",
+	"target_url": "https://example.com/build/status",
+	"description": "Build started",
+	"context": "continuous-integration/appcenter"
+}'
