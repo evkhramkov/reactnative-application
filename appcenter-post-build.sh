0f6e4ad8f3074710f3ad089f1edb61a3de0bb4b5
Evgeniy Khramkov <evkhramk@microsoft.com>
2018-03-20 17:42:51 +0300

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,1 +1,13 @@
+npm test
 
+curl -X POST \
+  https://api.github.com/repos/evkhramkov/reactnative-application/statuses/$BUILD_SOURCEVERSION \
+  -H 'Authorization: token $GITHUB_TOKEN' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -d '{
+	"state": "success",
+	"target_url": "https://example.com/build/status",
+	"description": "Build started",
+	"context": "continuous-integration/appcenter"
+}'
